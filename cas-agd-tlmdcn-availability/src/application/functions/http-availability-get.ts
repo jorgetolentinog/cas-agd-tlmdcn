c28bdbd67b476c1f75f20f7eb691ce88c221bc75
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-10-08 17:57:46 -0500

    -

--- cas-agd-tlmdcn-availability/src/application/functions/http-availability-get.ts
+++ cas-agd-tlmdcn-availability/src/application/functions/http-availability-get.ts
@@ -1,8 +1,12 @@
 import { httpHandler } from "@/application/shared/http-handler";
+import { CalcAvailability } from "@/domain/usecase/calc-availability/CalcAvailability";
+import { container } from "tsyringe";
 
 export const handler = httpHandler(async (event) => {
+  const response = await container.resolve(CalcAvailability).execute();
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "OK" }),
+    body: JSON.stringify(response),
   };
 });
